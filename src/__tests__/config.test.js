f088d8ff8ce7a4950abdac08b4c1fa634c6429e0
Ty Cobb <ty.cobb.m@gmail.com>
2017-04-01 17:45:06 -0500

    setup test suite; add config tests

---
+++ src/__tests__/config.test.js
@@ -0,0 +1,54 @@
+// @flow
+/* eslint-env jest */
+import { loadConfig } from '../config'
+
+// subject
+let subject
+
+// specs
+describe('in development', () => {
+  beforeEach(() => {
+    process.env.ENVIRONMENT = 'development'
+    subject = loadConfig()
+  })
+
+  it('points to localhost', () => {
+    expect(subject.graphUrl).toEqual('http://localhost:5000/graphql')
+  })
+})
+
+describe('in staging', () => {
+  beforeEach(() => {
+    process.env.ENVIRONMENT = 'staging'
+    subject = loadConfig()
+  })
+
+  it('points to the staging site', () => {
+    expect(subject.graphUrl).toEqual('https://witness-slips.herokuapp.com/graphql')
+  })
+})
+
+describe('in production', () => {
+  beforeEach(() => {
+    process.env.ENVIRONMENT = 'production'
+    subject = loadConfig()
+  })
+
+  it('points to the production site', () => {
+    expect(subject.graphUrl).toEqual('https://witness-slips.herokuapp.com/graphql')
+  })
+})
+
+describe('when there is no environment specified', () => {
+  it('raises an error', () => {
+    delete process.env.ENVIRONMENT
+    expect(loadConfig).toThrow('No environment specified!')
+  })
+})
+
+describe('when there is no associated config', () => {
+  it('raises an error', () => {
+    process.env.ENVIRONMENT = 'foo'
+    expect(loadConfig).toThrow('No config for environment: foo!')
+  })
+})
