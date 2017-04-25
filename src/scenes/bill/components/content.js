753356f22bb76c8edb347e42d22d4a5c48b43a0b
Ty Cobb <ty.cobb.m@gmail.com>
2017-04-25 16:29:26 -0500

    Add mobile styles (#36)
    * add mobile styles for bill cell
    * added mobile styles to bill content
    * properly support mobile / iphone5
    * fix travis build

--- src/scenes/bill/components/content.js
+++ src/scenes/bill/components/content.js
@@ -5,7 +5,7 @@ import moment from 'moment'
 import { StyleSheet, css } from 'aphrodite/no-important'
 import { Element } from '../components'
 import { Button } from 'shared/components'
-import { colors } from 'shared/styles'
+import { borders, colors, utils } from 'shared/styles'
 import type { Bill } from 'shared/types'
 
 const { floor } = Math
@@ -26,7 +26,7 @@ class Content extends Component {
 
     return <div>
       <div className={css(styles.header)}>
-        <h1>{bill.title}</h1>
+        {bill.title && <h1 className={css(styles.title)}>{bill.title}</h1>}
         <h4>{bill.documentNumber}</h4>
       </div>
       <div className={css(styles.body)}>
@@ -34,7 +34,7 @@ class Content extends Component {
           <Element label='State Synopsis'><p>{bill.summary}</p></Element>
         </div>
         <div className={css(styles.spacer)} />
-        <div className={css(styles.column)}>
+        <div className={css(styles.column, styles.lastColumn)}>
           <Element style={styles.date} label='Hearing Date'>
             <p>
               <span>{date.calendar()}</span>
@@ -47,9 +47,25 @@ class Content extends Component {
         </div>
       </div>
       <div className={css(styles.actions)}>
-        <Button style={styles.link} to={bill.witnessSlipUrl} label='Take Action' iconName='pencil-square-o' type='solid' />
-        <Button style={styles.link} to={bill.detailsUrl} label='View Details' iconName='info-circle' />
-        <Button to={bill.fullTextUrl} label='View Bill' iconName='file-text-o' />
+        <Button
+          style={styles.button}
+          to={bill.witnessSlipUrl}
+          label='Take Action'
+          iconName='pencil-square-o'
+          type='solid' />
+        <div className={css(styles.secondaryActions)}>
+          <Button
+            style={styles.button}
+            to={bill.detailsUrl}
+            label='View Details'
+            iconName='info-circle' />
+          <div className={css(styles.mobileSpacer)} />
+          <Button
+            style={styles.button}
+            to={bill.fullTextUrl}
+            label='View Bill'
+            iconName='file-text-o' />
+        </div>
       </div>
     </div>
   }
@@ -57,15 +73,35 @@ class Content extends Component {
 
 const styles = StyleSheet.create({
   header: {
+    display: 'flex',
+    flexDirection: 'column',
     marginBottom: 15
+  },
+  title: {
+    ...utils.mobile({
+      marginBottom: 5
+    })
   },
   body: {
+    ...borders.low(['bottom']),
     display: 'flex',
     flexBasis: 0,
-    marginBottom: 15
+    paddingBottom: 15,
+    marginBottom: 15,
+    ...utils.mobile({
+      flexDirection: 'column'
+    })
   },
   column: {
-    flex: 1
+    flex: 1,
+    ...utils.mobile({
+      marginBottom: 15
+    })
+  },
+  lastColumn: {
+    ...utils.mobile({
+      marginBottom: 0
+    })
   },
   date: {
     marginBottom: 10
@@ -78,10 +114,29 @@ const styles = StyleSheet.create({
     width: 30
   },
   actions: {
-    display: 'flex'
+    display: 'flex',
+    ...utils.mobile({
+      flexDirection: 'column',
+      alignItems: 'stretch'
+    })
   },
-  link: {
-    marginRight: 10
+  secondaryActions: {
+    display: 'flex',
+    ...utils.mobile({
+      marginTop: 10
+    })
+  },
+  button: {
+    marginRight: 10,
+    ...utils.mobile({
+      flex: 1,
+      marginRight: 0
+    })
+  },
+  mobileSpacer: {
+    ...utils.mobile({
+      width: 15
+    })
   }
 })
 
