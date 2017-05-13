ca638bb5984d849968d8029d8a47e48cf47e7486
Ty Cobb <ty.cobb.m@gmail.com>
2017-05-13 15:56:38 -0500

    remove unused flowtyped defs

--- flow-typed/npm/react-notification_vx.x.x.js
+++
@@ -1,6 +0,0 @@
-// flow-typed signature: 7b7a07d04ccf1dd5c951d82a00cc1fd8
-// flow-typed version: <<STUB>>/react-notification_v6.7.0/flow_v0.42.0
-
-declare module 'react-notification' {
-  declare module.exports: any;
-}
