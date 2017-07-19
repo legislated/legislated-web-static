9acc9be2a537b08e0331cac73b46fd1db01b6c17
Ty Cobb <ty.cobb.m@gmail.com>
2017-07-18 19:28:25 -0500

    install watchman into the home directory

--- scripts/ci-watchman.sh
+++ scripts/ci-watchman.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/facebook/watchman.git
-pushd watchman
+git clone https://github.com/facebook/watchman.git ~/facebook/watchman
+pushd ~/facebook/watchman
 git checkout v4.7.0
 ./autogen.sh && ./configure && make && sudo make install
 popd
