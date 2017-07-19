ec2bf2d927a56479789c0dc183080a2a738f5b54
Ty Cobb <ty.cobb.m@gmail.com>
2017-07-19 14:53:08 -0500

    just don't cache watchman

--- scripts/ci-watchman.sh
+++ scripts/ci-watchman.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e
 
-WATCHMAN_PATH=~/facebook/watchman
-
-if [ ! -d $WATCHMAN_PATH ]; then
-  git clone https://github.com/facebook/watchman.git $WATCHMAN_PATH
-  pushd $WATCHMAN_PATH
-  git checkout v4.7.0
-  ./autogen.sh && ./configure && make && sudo make install
-  popd
-fi
+git clone https://github.com/facebook/watchman.git $WATCHMAN_PATH
+pushd $WATCHMAN_PATH
+git checkout v4.7.0
+./autogen.sh && ./configure && make && sudo make install
+popd
